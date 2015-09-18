3058ea429e28188cdd322b091d4dd80ef8e799eb
Andrew Head <head.andrewm@gmail.com>
2015-09-18 12:14:18 -0700

    Revert "Tutorons: Added wrapper server for serving SVGs"
    This reverts commit 13018453d1f740fe509dc8c6bbfd1b8d10c98777.

--- gulpfile.js
+++ gulpfile.js
@@ -15,13 +15,6 @@ gulp.task('default', ['server', 'docs'], function() {
   gulp.watch(config.globs.js, ['browserify', 'docs']);
 });
 
-gulp.task('svg-server', ['server'], function() {
-  var REGEXPER_ADDR = 'http://127.0.0.1:8080';
-  var SVG_SERVER_PORT = 8000;
-  var svgServer = require('./svg_server');
-  svgServer.start(SVG_SERVER_PORT, REGEXPER_ADDR);
-});
-
 gulp.task('docs', ['docs:files'], function() {
   var folderToc = require('folder-toc');
 
